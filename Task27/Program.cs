08d85a2e57f0747b58fa6e14399745e203c1205e
kovalevandrey007 <kovalevandrey007@gmail.com>
2022-12-05 23:49:03 +0200

    finish_save

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,17 +1,31 @@
-﻿/* Задача 27: Напишите программу, которая принимает 
+﻿
+/* Задача 27: Напишите программу, которая принимает 
 на вход число и выдаёт сумму цифр в числе.
 
 452 -> 11
 82 -> 10
 9012 -> 12*/
 
-Console.WriteLine("введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int count = 0; // количество цифр в числе
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+//string stringNum = num.ToString();
+int sum = 0;
+while (num > 0)
+    {
+    sum = sum+num%10;
+    num/=10;
+    }
+Console.WriteLine($"сумма чисел равна={sum}");
 
-while (a > 0)
+
+
+    
+ 
+
+/*int sum = 0;
+while (num != 0)
 {
-    a = a/10;
-    count++;
+   a = a+count(i);
+   int summ = a + acount(i);
 }
-Console.WriteLine("количество цифр = " + count);
+Console.WriteLine("сумма цифр = " + summ);*/
--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,33 +1,14 @@
-﻿using System;
-/* Задача 29: Напишите программу, которая задаёт 
+﻿/* Задача 29: Напишите программу, которая задаёт 
 массив из 8 элементов и выводит их на экран.
 
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
-
-int [] array = new int[8];
-new string= ConvertToString(new int[8]);
-
-
-//new array[] = Convert.ToInt32(new int[]);
-
-Console.WriteLine($"Ведите числа 8 элементов: {new string}, ");
-Console.ReadLine();
-for (int i = 0; i < array.Length; i++)
+            
+int[] arr = new int[8];
+Console.WriteLine("Введите 8 (Восемь) целых чисел для создания массива");
+for (int i = 0; i < arr.Length; i++)
 {
-    array[i] = new string;
-    Console.WriteLine($"{array[i]}");
+arr[i] = Convert.ToInt32(Console.ReadLine());
 }
-
-
-
-/*void PrintArray (int[] array)
-{
-    int count = array.Length;
-
-    for(int i = 0; i<count; i++ )
-    {
-        Console.Write($"{array} ");
-    }
-    Console.WriteLine();
-}*/
+Console.WriteLine("Вы создали массив:");
+Console.Write(string.Join(", ", arr));
